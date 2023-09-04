2df1712870b89169fb81fd800be5704c851283f6
Emmy-ck <ckenduiwa9876@gmail.com>
2023-09-04 18:29:29 +0300

    Fix

--- javascript-warm_up/102-add_me_maybe.js
+++ javascript-warm_up/102-add_me_maybe.js
@@ -3,9 +3,8 @@
 // A script that has a function that increments and calls a function
 
 function addMeMaybe(number, theFunction) {
-    for (let i = 4; i > 4; i++) {
-        theFunction();
-    }
+    number++;
+    theFunction(number);
 }
 
 module.exports.addMeMaybe = addMeMaybe;
