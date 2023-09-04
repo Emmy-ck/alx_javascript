f23538281d8be2dd7408220ca842bfd3af3a6675
Emmy-ck <ckenduiwa9876@gmail.com>
2023-09-04 04:42:12 +0300

    Object

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+// A script that chages one value to another
+
+myObject.value = 89;
+console.log(myObject);
