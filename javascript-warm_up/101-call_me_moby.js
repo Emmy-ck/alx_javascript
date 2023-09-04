8c4b5ba817f3ea99868df69e317f2170c3b33485
Emmy-ck <ckenduiwa9876@gmail.com>
2023-09-04 18:02:55 +0300

    Call me Moby

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -3,7 +3,7 @@
 // A script with a function that executes x times a function
 
 function callMeMoby() {
-    console.log(callMeMoby);
+    console.log('C is fun');
 }
 
 module.exports.callMeMoby = callMeMoby;
