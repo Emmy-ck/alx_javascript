63e96c762ffab3dbd613597ce9ae862cb1863043
Emmy Kenduiwa <ckenduiwa9876@gmail.com>
2023-09-03 18:32:10 -0700

    Semistandard validation fix

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -2,8 +2,8 @@
 
 // A script that pints 3 lines by using an array of string and a loop
 
-let printWords = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+const printWords = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < printWords.length; i++) {
-    console.log(printWords[i])
+  console.log(printWords[i]);
 }
