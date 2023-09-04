1bda92c8bbd2c53759434e29fa8569b2f7e6e35c
Emmy-ck <ckenduiwa9876@gmail.com>
2023-09-04 19:08:19 +0300

    Minor fix

---
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// Creating an empty class Rectangle that defines a rectangle.
+
+class Rectangle {
+
+}
+
+// Exporting this class to be imported and used in other files
+
+module.exports = Rectangle;
