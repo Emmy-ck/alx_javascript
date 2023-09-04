7d90fa43bb2abadf97228f6e540a09d0081c5176
Emmy-ck <ckenduiwa9876@gmail.com>
2023-09-04 04:29:26 +0300

    Loop to all languages

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// A script that pints 3 lines by using an array of string and a loop
+
+let printWords = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+
+for (let i = 0; i < printWords.length; i++) {
+    console.log(printWords[i])
+}
