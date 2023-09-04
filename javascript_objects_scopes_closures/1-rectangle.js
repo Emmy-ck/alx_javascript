781f9032704096492076e52899859d9fed9af04c
Emmy-ck <ckenduiwa9876@gmail.com>
2023-09-04 19:21:56 +0300

    Rectangle #1

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+// Creating an empty class Rectangle that defines a rectangle.
+
+class Rectangle {
+    // Constructor taking 2 arguments
+    constructor(w, h) {
+        // Initializing instance attributes width and height
+        this.width: w;
+        this.height: h;
+    }
+}
+
+// Exporting this class to be imported and used in other files
+
+module.exports = Rectangle;
