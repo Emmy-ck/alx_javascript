05219f631c1038338ad575e97f4e76f184ff3a15
Emmy-ck <ckenduiwa9876@gmail.com>
2023-10-11 13:32:51 +0300

    Private methods with closure

---
+++ Javascript-advanced/3-classroom.js
@@ -0,0 +1,17 @@
+// Functions handling closure and loops
+
+function createClassRoom(numberOfStudents) {
+    function studentSeat(seat) {
+        return function () {
+            return seat;
+        };
+    }
+    var students = [];
+
+    for (var i = 0; i < numberOfStudents; i++) {
+        students.push(studentSeat(i + 1));
+    }
+    return students;
+}
+
+var classRoom = createClassRoom(10);
